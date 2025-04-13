b6d343548c7aadc5aea2407f93dc560b3e7c9e7c
bdm77 <bdm77@DESKTOP-5C808VG>
2025-04-13 17:06:57 +0300

    Add project files.

---
+++ Pianoo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pianoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press a number key (1–8) to play a note from C to C:");
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(intercept: true);
+                char ch = key.KeyChar;
+                Piano(ch);
+            }
+        }
+
+
+        static void Piano(char ch)
+        {
+            int duration = 300;
+
+            if (ch == '1')
+            {
+                Console.Beep(261, duration); // C4
+            }
+            if (ch == '2')
+            {
+                Console.Beep(293, duration); // D
+            }
+            if (ch == '3')
+            {
+                Console.Beep(329, duration); // E
+            }
+            if (ch == '4')
+            {
+                Console.Beep(349, duration); // F
+            }
+            if (ch == '5')
+            {
+                Console.Beep(392, duration); // G
+            }
+            if (ch == '6')
+            {
+                Console.Beep(440, duration); // A
+            }
+            if (ch == '7')
+            {
+                Console.Beep(493, duration); // B
+            }
+            if (ch == '8')
+            {
+                Console.Beep(523, duration); // C5 (one octave higher)
+            }
+        }
+    }
+}
